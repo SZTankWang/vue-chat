ad3e9184ec257d0f8ed34ee4e119b16754c44183
SZTankWang <2938599137@qq.com>
2023-03-29 00:50:16 -0500

    ws

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 export let serverURL:string = "https://api.zhenmingwang.online/api/chat";
 
-export let chatURL:string = "wss://api.zhenmingwang.online/api/chat/chat";
+export let chatURL:string = "wss://api.zhenmingwang.online/api/websocket";
 export let secretPhrase:string = "myphrase"
